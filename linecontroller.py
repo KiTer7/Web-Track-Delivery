078739605eea45f0d34fd9fcd08814a0e2b1484d
gbaites <140131720+gbaites@users.noreply.github.com>
2023-08-19 17:35:35 +1000

    changed some things

--- linecontroller.py
+++ linecontroller.py
@@ -1,33 +1,32 @@
 import json
 
 # Define your coordinates using variables
-coordinates_Hobart = [10, 20]
-coordinates_Launceston = [10, 10]
-coordinates_Melbourne = [20, 20]
-coordinates_Sydney = []
-coordinates_Canberra = []
+coordinates_Hobart = [147.33838, -42.88239]
+coordinates_Launceston = [147.13255, -41.43418]
+coordinates_Melbourne = [144.93271, -37.84096]
+coordinates_Sydney = [151.20951, -33.96790]
+coordinates_Canberra = [149.19393, -35.30751]
 
+coordinates = [[147.33838,-42.88239], [147.13255,-41.43418], [144.93271,-37.84096], [151.20951,-33.96790], [149.19393,-35.30751]]
+coordinate_pairs = []
+for coords in coordinates:
+    temp_list = coordinates.copy()
+    temp_list.remove(coords)
+    for second_coords in temp_list:
+        coordinate_pairs.append([coords, second_coords])
+    coordinates.remove(coords)
+    
 
 # Create a dictionary representing the GeoJSON structure
+features = [{"type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": coordinate_pair
+            }} for coordinate_pair in coordinate_pairs]
+
 geojson = {
     "type": "FeatureCollection",
-    "features": [
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "LineString",
-                "coordinates": [coordinates_Hobart, coordinates_Launceston]
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "LineString",
-                "coordinates": [coordinates_Hobart, coordinates_Melbourne]
-            }
-        }
-        # Add more features as needed
-    ]
+    "features": features
 }
 
 # Save the GeoJSON data to a file
